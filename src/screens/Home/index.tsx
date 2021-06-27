9752b64c06f88195340fffa677c2f3aa84d81b8d
Renan  Melo <renan.melo@wapstore.com.br>
2021-06-27 10:04:41 -0300

    add header on home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
-import { Text } from "react-native";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Logo from "../../assets/logo.svg";
+
 import * as S from "./styles";
 
 export function Home() {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <S.Header>
+        <S.HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+
+          <S.TotalCars>Total de 12 carros</S.TotalCars>
+        </S.HeaderContent>
+      </S.Header>
+    </S.Container>
+  );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,8 +1,32 @@
-import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
+
   background-color: ${({ theme }) => theme.colors.background_primary};
+`;
+
+export const Header = styled.View`
+  ${({ theme }) => css`
+    width: 100%;
+    height: 113px;
+    background-color: ${theme.colors.header};
+    justify-content: flex-end;
+    padding: 32px 24px;
+  `}
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TotalCars = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${RFValue(15)}px;
+    font-family: ${theme.fonts.primary_400};
+    color: ${theme.colors.text};
+  `}
 `;
